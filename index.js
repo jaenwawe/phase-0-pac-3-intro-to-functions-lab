13211068b6fe06f1a7b52c16858443337a3ef9c4
jaenwawe <nwawe.jae@gmail.com>
2021-07-25 10:17:39 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+
+function shout (string)
+{
+ 
+  return 'Hello'.toUpperCase();
+}
+
+function whisper (string){
+    return 'HEllo'.toLowerCase();
+
+}
+
+function logWhisper (string){
+    console.log('HEllo'.toLowerCase());
+
+}
+
+function logWhisper (string){
+    console.log('HEllo'.toLowerCase());
+
+}
+
+function  logShout(string){
+    console.log('HEllo'.toUpperCase());
+}
+
+
+function  shout(string){
+    return string.toUpperCase();
+}
+
+function sayHiToGrandma(string){
+  
+    console.log(string);
+
+    if (string  === "I love you, Grandma." )
+        return ("I love you, too.");
+        
+    if (string === string.toUpperCase())
+        return("YES INDEED!");
+
+    if (string === string.toLowerCase())
+    return ("I can't hear you!");
+    
+   
+ 
+}
